0cb128ad5404d96fb916fc20f32c5bcfbc235a56
Evan Burton <evan@heyevan.com>
2020-04-06 04:53:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(current_time)
-  current_time = 10
-while current_time > 1
+  counter = 10
+while counter > 1
   puts "#{current_time} SECONDS"
- current_time -= 1
+ counter -= 1
  "HAPPY NEW YEAR!"
   end
 end
